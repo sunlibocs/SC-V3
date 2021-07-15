4c7849b867ad93c202b7de2a58a7ea246bba788e
sunlibocs <sunlibocs@gmail.com>
2021-07-15 21:13:01 +0930

    xxxx

--- ranking_loss.py
+++ ranking_loss.py
@@ -11,7 +11,7 @@ class MaskRanking_Loss(nn.Module):
         self.sample_ratio = sample_ratio
         self.filter_depth = filter_depth
 
-    def generate_target(self, depth, pred, tgt_valid_weight, theta=0.02):
+    def generate_target(self, depth, pred, theta=0.02):
         B, C, H, W = depth.shape
         mask_A = torch.rand(C, H, W).cuda()
         mask_A[mask_A >= (1 - self.sample_ratio)] = 1
@@ -50,8 +50,8 @@ class MaskRanking_Loss(nn.Module):
         squared_loss = torch.mean(pred_depth[target == 0] ** 2)  # if pred depth is not zero adds to loss
         return log_loss + squared_loss
 
-    def forward(self, pred_depth, gt_depth, tgt_valid_weight):
-        za, zb, target = self.generate_target(gt_depth, pred_depth, tgt_valid_weight)
+    def forward(self, pred_depth, gt_depth):
+        za, zb, target = self.generate_target(gt_depth, pred_depth)
         total_loss = self.cal_ranking_loss(za, zb, target)
 
         return total_loss
