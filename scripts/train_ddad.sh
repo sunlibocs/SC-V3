69c84e2139a896018c50ddfe0e258e3805962d7b
sunlibocs <sunlibocs@gmail.com>
2021-07-27 14:53:26 +0930

    change epoch 200 to 10

--- scripts/train_ddad.sh
+++ scripts/train_ddad.sh
@@ -4,7 +4,7 @@ python train_org.py $TRAIN_SET \
 --folder-type sequence \
 --resnet-layers 18 \
 --num-scales 1 \
--b4 -s0.001 -c0.5 --epoch-size 1000 --epochs 200 \
+-b4 -s0.001 -c0.5 --epoch-size 1000 --epochs 100 \
 --with-ssim 1 \
 --with-mask 1 \
 --with-auto-mask 1 \
